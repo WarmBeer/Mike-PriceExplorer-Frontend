7b941c3fa32fc9ec478ed22ff08f7f0648cd8fd0
WarmBeer <mickvd99@gmail.com>
2020-07-06 22:31:54 +0200

    Added components

---
+++ src/components/LookupBar.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="lookup-bar">
+        <select id="currency" class="lookup-bar_option">
+            <option value="BTC">Bitcoin</option>
+        </select>
+        <select id="price-currency" class="lookup-bar_option">
+            <option value="EUR">€ Euro</option>
+            <option value="USD">$ US Dollar</option>
+        </select>
+        <input id="start-date" type="date">
+        <input id="end-date" type="date">
+        <button class="lookup-bar_confirm">GO</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "LookupBar"
+    }
+</script>
+
+<style scoped>
+    .lookup-bar {
+        display: block;
+    }
+
+    .lookup-bar_option {
+
+    }
+</style>
---
+++ src/components/LookupTable.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="lookup-table">
+        <table>
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Date</th>
+                    <th>Exchange</th>
+                    <th>Pair</th>
+                    <th>Price</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr v-for="(row, index) in this.response.data" :key="index">
+                    <td>{{ index + 1 }}</td>
+                    <td>{{ new Date(row.date).toDateString() }}</td>
+                    <td>{{ row.exchange }}</td>
+                    <td>{{ row.currency }}/{{ row.priceCurrency }}</td>
+                    <td>{{ row.price }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "LookupTable",
+        props: ['response'],
+        computed: {
+            prices() {
+                let prices = []
+                return prices;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .lookup-table {
+        margin: 1em 2em;
+        display: grid;
+    }
+</style>
