fd6d8d8083efeeaa07dd7c3d6bd32b50b9a01d9d
WarmBeer <mickvd99@gmail.com>
2020-07-06 22:31:27 +0200

    Concept

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Lookup msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Lookup from './components/views/Lookup.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Lookup
   }
 }
 </script>
 
 <style>
+  html, body {
+    margin: 0;
+    padding: 0;
+  }
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
